03c81a609452a95a0ba4cd3afe8fc4e95221e88d
Andy Fragen <andy@thefragens.com>
2014-11-24 16:16:11 -0800

    make conditional

--- debug-notices.php
+++ debug-notices.php
@@ -3,7 +3,7 @@
 Plugin Name:       Debug Notices
 Plugin URI:        https://github.com/afragen/debug-notices/
 Description:       This plugin is used for displaying specific debug data.
-Version:           0.1.0
+Version:           0.2.0
 Author:            Andy Fragen
 License:           GNU General Public License v2
 License URI:       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -12,11 +12,10 @@ GitHub Branch:     master
 */
 
 add_action( 'wp_head', 'fragen_debug_notices' );
-//add_action( 'admin_head', 'fragen_debug_notices' );
+add_action( 'admin_head', 'fragen_debug_notices' );
 
 function fragen_debug_notices() {
 	global $wp_query;
-	$current_screen = null;
 
 	if ( function_exists( 'get_current_screen' ) && isset( get_current_screen()->id ) ) {
 		$current_screen = get_current_screen()->id;
@@ -24,15 +23,24 @@ function fragen_debug_notices() {
 	if ( isset( $wp_query->query_vars['post_type'] ) ) {
 		$post_type = $wp_query->query_vars['post_type'];
 	}
-	if ( empty( $post_type ) ) {
+
+	if ( empty( $current_screen ) && empty( $post_type ) ) {
 		return false;
 	}
-
-	$script = "<script type='text/javascript'>
+	
+	if ( is_admin() ) {
+		$script = "<script type='text/javascript'>
+		jQuery(document).ready( function($) {
+			$('body').prepend('<br /><strong>Debug Notices</strong>";
+	} else {
+		$script = "<script type='text/javascript'>
 		jQuery(document).ready( function($) {
 			$('header').prepend('<br /><strong>Debug Notices</strong>";
-	$script .= '<p>Post Type: ' . $post_type . '</p>';
-	if ( $current_screen ) {
+	}
+	if ( ! empty( $post_type ) ) {
+		$script .= '<p>Post Type: ' . $post_type . '</p>';
+	}
+	if ( ! empty( $current_screen ) ) {
 		$script .= '<p>Current Screen: ' . $current_screen . '</p>';
 	}
 	$script .= "'); }); </script>";
